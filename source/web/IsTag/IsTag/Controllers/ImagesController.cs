dd603ff93fe7934d5a326ca6fc1dc08a9cfd6481
Andrea <andrea.appstore@gmail.com>
2019-07-20 00:15:55 +0200

    Gli sviluppatori scrivono codice

---
+++ source/web/IsTag/IsTag/Controllers/ImagesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IsTag.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IsTag.Controllers
+{
+    [AllowAnonymous]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImagesRepository _imagesRepository;
+        public ImagesController(IImagesRepository imagesRepository)
+        {
+            _imagesRepository = imagesRepository;
+        }
+
+
+        public IActionResult Image(string id)
+        {
+            var img = _imagesRepository.GetImage(id);
+            return File(img, "image/jpeg");
+        }
+    }
+}
---
+++ source/web/IsTag/IsTag/Repositories/IImagesRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IsTag.Repositories
+{
+    public interface IImagesRepository
+    {
+        byte[] GetImage(string id);
+    }
+}

f335199d7b2abd3399a890ee660587449fd33cc5
Francesco Bonizzi <fbonizzi@hotmail.it>
2019-07-19 20:41:43 +0200

    Consumable GetAll

--- source/web/IsTag/IsTag/Controllers/ConsumablesController.cs
+++ source/web/IsTag/IsTag/Controllers/ConsumablesController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 
 namespace IsTag.Controllers
 {
@@ -18,6 +15,34 @@ namespace IsTag.Controllers
             public string Name { get; set; }
             public string Status { get; set; }
         }
+
+        public IActionResult GetAll()
+        {
+            return Ok(new List<Consumable>()
+            {
+                new Consumable()
+                {
+                    Name = "PulisciCulo",
+                    Status = new Random().Next() % 2 == 0 ? "Missing" : "NotMissing"
+                },
+                new Consumable()
+                {
+                    Name = "PulisciFiga",
+                    Status = new Random().Next() % 2 == 0 ? "Missing" : "NotMissing"
+                },
+                new Consumable()
+                {
+                    Name = "PulisciCuore",
+                    Status = new Random().Next() % 2 == 0 ? "Missing" : "NotMissing"
+                },
+                new Consumable()
+                {
+                    Name = "PulisciAnima",
+                    Status = new Random().Next() % 2 == 0 ? "Missing" : "NotMissing"
+                },
+            });
+        }
+
         public IActionResult GetData(string id)
         {
             return Ok(new Consumable()

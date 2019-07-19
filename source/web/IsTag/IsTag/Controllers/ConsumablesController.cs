519bf2950fe948523d402df877bfe931074ba633
Francesco Bonizzi <fbonizzi@hotmail.it>
2019-07-19 21:46:55 +0200

    Consumable - All objects

--- source/web/IsTag/IsTag/Controllers/ConsumablesController.cs
+++ source/web/IsTag/IsTag/Controllers/ConsumablesController.cs
@@ -13,7 +13,30 @@ namespace IsTag.Controllers
         public class Consumable
         {
             public string Name { get; set; }
+            public string QRCode { get; set; }
             public string Status { get; set; }
+            public string Description { get; set; }
+
+            public bool IsMissing
+            {
+                get
+                {
+                    return Status == "Missing";
+                }
+
+                set
+                {
+                    if (value)
+                    {
+                        Status = "Missing";
+                    }
+                    else
+                    {
+                        Status = "NotMissing";
+                    }
+                }
+            }
+
         }
 
         public IActionResult GetAll()
@@ -23,22 +46,30 @@ namespace IsTag.Controllers
                 new Consumable()
                 {
                     Name = "PulisciCulo",
-                    Status = new Random().Next() % 2 == 0 ? "Missing" : "NotMissing"
+                    QRCode = "a",
+                    Status = new Random().Next() % 2 == 0 ? "Missing" : "NotMissing",
+                    Description = "Lorem ipsum bla bla bla bla bla bla bla bla bla bla bla bla bla bla"
                 },
                 new Consumable()
                 {
                     Name = "PulisciFiga",
-                    Status = new Random().Next() % 2 == 0 ? "Missing" : "NotMissing"
+                    QRCode = "b",
+                    Status = new Random().Next() % 2 == 0 ? "Missing" : "NotMissing",
+                    Description = "Lorem ipsum bla bla bla bla bla bla bla bla bla bla bla bla bla bla"
                 },
                 new Consumable()
                 {
                     Name = "PulisciCuore",
-                    Status = new Random().Next() % 2 == 0 ? "Missing" : "NotMissing"
+                    QRCode = "c",
+                    Status = new Random().Next() % 2 == 0 ? "Missing" : "NotMissing",
+                    Description = "Lorem ipsum bla bla bla bla bla bla bla bla bla bla bla bla bla bla"
                 },
                 new Consumable()
                 {
                     Name = "PulisciAnima",
-                    Status = new Random().Next() % 2 == 0 ? "Missing" : "NotMissing"
+                    QRCode = "d",
+                    Status = new Random().Next() % 2 == 0 ? "Missing" : "NotMissing",
+                    Description = "Lorem ipsum bla bla bla bla bla bla bla bla bla bla bla bla bla bla"
                 },
             });
         }

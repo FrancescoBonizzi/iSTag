83d7865faceba11dbbd77bed2ce86538d0926131
Andrea <andrea.appstore@gmail.com>
2019-07-19 17:48:45 +0200

    Controller generico

---
+++ source/web/IsTag/IsTag/Controllers/GenericObjectApiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IsTag.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenericObjectApiController : ControllerBase
+    {
+        
+        public IActionResult Get(string id)
+        {
+            return Ok("Warehouse");
+        }
+    }
+}

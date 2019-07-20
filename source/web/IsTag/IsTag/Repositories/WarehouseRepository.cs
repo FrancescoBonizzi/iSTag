336d8450136e52a6cd86abe2e831d0db70e1ca83
Andrea <andrea.appstore@gmail.com>
2019-07-20 09:48:12 +0200

    warehouse

--- source/web/IsTag/IsTag/Repositories/WarehouseRepository.cs
+++ source/web/IsTag/IsTag/Repositories/WarehouseRepository.cs
@@ -20,6 +20,7 @@ namespace IsTag.Repositories
         }
 
         
+        
 
         public class WarehouseItemTemp
         {
@@ -35,7 +36,7 @@ namespace IsTag.Repositories
         {
             using (var connection = new SqlConnection(_connectionString))
             {
-                var wh = connection.Query<WarehouseItemTemp>("SELECT Q.QRCode, W.Name, W.Category, W.Description, W.ImageCode, U.Name as OwnerName, U.Email as OwnerEmail FROM Warehouse W INNER JOIN QRCodes Q ON W.QRCodeID = Q.QRCodeID LEFT JOIN Users U ON U.UserID = (SELECT TOP 1 UserID FROM WarehouseUpdates WU WHERE WU.QRCodeID = Q.QRCodeID)");
+                var wh = connection.Query<WarehouseItemTemp>("SELECT Q.QRCode, W.Name, W.Category, W.Description, W.ImageCode, U.Name as OwnerName, U.Email as OwnerEmail FROM Warehouse W INNER JOIN QRCodes Q ON W.QRCodeID = Q.QRCodeID LEFT JOIN Users U ON U.UserID = (SELECT TOP 1 UserID FROM WarehouseUpdates WU WHERE WU.QRCodeID = Q.QRCodeID) ORDER BY Q.QRCodeID DESC");
                 return wh.Select(a => new WarehouseItem()
                 {
                     Category = a.Category,
@@ -82,6 +83,58 @@ namespace IsTag.Repositories
                         Description = warehouseItem.Description,
                         ImageCode = warehouseItem.ImageCode
                     });
+            }
+        }
+
+        public class OwnershipTemp
+        {
+            public string QRCode { get; set; }
+            public DateTime When { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+        }
+        public IEnumerable<Ownership> GetOwnershipHistoryOfItem(string id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var tmp = connection.Query<OwnershipTemp>("SELECT Q.QRCode, WU.[When], U.Name, U.Email FROM QRCodes Q INNER JOIN WarehouseUpdates WU ON Q.QRCodeID = WU.QRCodeID LEFT JOIN Users U ON WU.UserID = U.UserID WHERE Q.QRCode = @Id ORDER BY WU.[When] DESC", new { Id = id });
+                return tmp.Select(a => new Ownership()
+                {
+                    QRCode = a.QRCode,
+                    When = a.When,
+                    UserData = a.Name != null || a.Email != null ? new Ownership.Owner()
+                    {
+                        Email = a.Email,
+                        Name = a.Name
+                    } : null
+                });
+            }
+        }
+
+
+        public IEnumerable<Ownership> GetOwnershipHistoryOfUser(string id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var tmp = connection.Query<OwnershipTemp>("SELECT Q.QRCode, WU.[When], U.Name, U.Email FROM QRCodes Q INNER JOIN WarehouseUpdates WU ON Q.QRCodeID = WU.QRCodeID LEFT JOIN Users U ON WU.UserID = U.UserID WHERE U.Email = @Email OR (U.Email IS NULL AND @Email = (SELECT TOP 1 U2.Email FROM Users U2 INNER JOIN WarehouseUpdates WU2 ON U2.UserID = WU2.UserID WHERE WU2.[When] < WU.[When] AND WU.QRCodeID = WU2.QRCodeId ORDER BY WU2.[When] DESC)) ORDER BY WU.[When] DESC", new { Email = id });
+                return tmp.Select(a => new Ownership()
+                {
+                    QRCode = a.QRCode,
+                    When = a.When,
+                    UserData = a.Name != null || a.Email != null ? new Ownership.Owner()
+                    {
+                        Email = a.Email,
+                        Name = a.Name
+                    } : null
+                });
+            }
+        }
+
+        public void Give(string what, string who)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Execute("INSERT INTO WarehouseUpdates SELECT QRCodeID = (SELECT QRCodeID FROM QRCodes WHERE QRCode = @What), UserID = (SELECT UserID FROM Users WHERE Email = @Who), SYSDATETIME()", new { Who = who, What = what });
             }
         }
     }

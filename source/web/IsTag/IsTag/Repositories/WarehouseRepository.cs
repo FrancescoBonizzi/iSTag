f19f9b37e4201016e492fffe927fdca16f3e61ff
Andrea <andrea.appstore@gmail.com>
2019-07-20 02:34:56 +0200

    the dog is on the table

--- source/web/IsTag/IsTag/Repositories/WarehouseRepository.cs
+++ source/web/IsTag/IsTag/Repositories/WarehouseRepository.cs
@@ -18,11 +18,46 @@ namespace IsTag.Repositories
         {
             _connectionString = JsonConvert.DeserializeObject<ConnectionStrings>(File.ReadAllText("cs.json")).ISTag;
         }
+
+        public class WarehouseItemTemp
+        {
+            public string OwnerName { get; set; }
+            public string OwnerEmail { get; set; }
+            public string QRCode { get; set; }
+            public string Name { get; set; }
+            public string Category { get; set; }
+            public string Description { get; set; }
+            public string ImageCode { get; set; }
+        }
+        public IEnumerable<WarehouseItem> GetAll()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var wh = connection.Query<WarehouseItemTemp>("SELECT Q.QRCode, W.Name, W.Category, W.Description, W.ImageCode, U.Name as OwnerName, U.Email as OwnerEmail FROM Warehouse W INNER JOIN QRCodes Q ON W.QRCodeID = Q.QRCodeID LEFT JOIN Users U ON U.UserID = (SELECT TOP 1 UserID FROM WarehouseUpdates WU WHERE WU.QRCodeID = Q.QRCodeID)");
+                return wh.Select(a => new WarehouseItem()
+                {
+                    Category = a.Category,
+                    Description = a.Description,
+                    ImageCode = a.ImageCode,
+                    Name = a.Name,
+                    QRCode = a.QRCode,
+                    CurrentOwner = a.OwnerEmail != null && a.OwnerName != null ?
+                        new WarehouseItem.Owner()
+                        {
+                            Email = a.OwnerEmail,
+                            Name = a.OwnerName
+                        }
+                        :
+                        null
+                }).ToArray();
+            }
+        }
+
         public WarehouseItem GetWarehouseItem(string id)
         {
             using (var connection = new SqlConnection(_connectionString))
             {
-                var wh = connection.Query<WarehouseItem>("SELECT W.Name, W.Category, W.Description, W.ImageCode FROM Warehouse W INNER JOIN QRCodes Q ON W.QRCodeID = Q.QRCodeID WHERE Q.QRCode = @Qr", new { Qr = id }).FirstOrDefault();
+                var wh = connection.Query<WarehouseItem>("SELECT Q.QRCode, W.Name, W.Category, W.Description, W.ImageCode FROM Warehouse W INNER JOIN QRCodes Q ON W.QRCodeID = Q.QRCodeID WHERE Q.QRCode = @Qr", new { Qr = id }).FirstOrDefault();
                 var up = connection.Query<WarehouseItem.Owner>("SELECT U.Name, U.Email FROM Users U WHERE U.UserID = (SELECT TOP 1 UserID FROM WarehouseUpdates WHERE QRCodeID = (SELECT QRCodeID FROM QRCodes WHERE QRCode = @Id) ORDER BY [When] DESC)", new { Id = id }).FirstOrDefault();
 
                 wh.CurrentOwner = up;
